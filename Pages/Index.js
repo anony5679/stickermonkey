797c676734806f0adba4de9dfb5a97d481d2b283
anony5679 <puneet.k6218@gmail.com>
2025-10-05 17:10:57 +0530

    Delete Pages directory

--- Pages/Index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{textAlign: "center", marginTop: "50px"}}>
-      <h1>🚀 My Next.js Website is Live!</h1>
-      <p>Deployed successfully on Vercel ✅</p>
-    </div>
-  )
-  }
