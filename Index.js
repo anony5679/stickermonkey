f46816ea131d54ecca04bea411d507ba34d5b0d2
anony5679 <puneet.k6218@gmail.com>
2025-10-05 15:21:18 +0530

    Create Index.js
    ..

---
+++ Index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Hello, Stickermonkey is live!</h1>
+      <p>If you can see this, your Next.js app is deployed correctly.</p>
+    </div>
+  );
+}
