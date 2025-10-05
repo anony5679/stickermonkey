127eb38baa02eb73fbe2e9441792928884b75a45
anony5679 <puneet.k6218@gmail.com>
2025-10-05 15:29:45 +0530

    Index.js
     .

---
+++ Pages/Index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>🚀 My Next.js Website is Live!</h1>
+      <p>Deployed successfully on Vercel ✅</p>
+    </div>
+  )
+  }
